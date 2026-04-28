8281cd0b97fee4fc2a3ff9c4c290c4656623113f
steverrfab <stevem@rrfabrication.org>
2026-04-28 15:42:20 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton
-} from "@clerk/nextjs";
+import { ClerkProvider, OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,13 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang="en">
         <body>
           <div className="auth-bar">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <OrganizationSwitcher hidePersonal />
+            <UserButton />
           </div>
           {children}
         </body>

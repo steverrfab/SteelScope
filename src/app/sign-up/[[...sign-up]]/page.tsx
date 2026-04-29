be813cacad878c03609b644027e132e2454f05a7
steverrfab <stevem@rrfabrication.org>
2026-04-28 21:23:19 -0400

    Create page.tsx

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <main className="auth-page">
+      <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" />
+    </main>
+  );
+}
